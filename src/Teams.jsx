611e73296c97722ab66312e700a337c3b17ecb2e
Parthib Biswas <162007516+ParthibBiswas10@users.noreply.github.com>
2025-03-03 00:41:31 +0530

    Update Teams.jsx

--- src/Teams.jsx
+++ src/Teams.jsx
@@ -2,7 +2,18 @@ import "./App.css";
 import Card from "./Card.jsx";
 function Team() {
   return (
-   
+   <>
+      <div className="app">
+        <main className="main-content">
+          <h1 className="title">Meet Our Team</h1>
+          <div className="TeamCard">
+            <Card
+              name="Unmesh Ghosh"
+              pic="Unmesh.jpg"
+              role="Team Lead"
+              linkedin="https://www.linkedin.com/in/unmesh-ghosh/"
+              github="https://github.com/Unmesh100"
+            />
   );
 }
 
